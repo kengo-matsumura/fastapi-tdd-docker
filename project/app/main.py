61069a24f1eea75ebc14777f789c163d6f41a7f0
Kengo Matsumura <kengo.matsumura@outlook.com>
2021-11-06 22:42:16 +1100

    Added POST route and updated files as required. Test cases also added.

--- project/app/main.py
+++ project/app/main.py
@@ -1,6 +1,6 @@
 import logging
 from fastapi import FastAPI
-from app.api import ping
+from app.api import ping, summaries
 from app.db import init_db
 
 log = logging.getLogger("uvicorn")
@@ -9,6 +9,11 @@ log = logging.getLogger("uvicorn")
 def create_application() -> FastAPI:
     application = FastAPI()
     application.include_router(ping.router)
+    application.include_router(
+        summaries.router,
+        prefix = "/summaries",
+        tags = ["summaries"]
+    )
     return application
 
 app = create_application()
--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -3,6 +3,7 @@ import pytest
 from starlette.testclient import TestClient
 from app.main import create_application
 from app.config import get_settings, Settings
+from tortoise.contrib.fastapi import register_tortoise
 
 
 def get_settings_override():
@@ -13,6 +14,24 @@ def test_app():
     # Set up
     app = create_application()
     app.dependency_overrides[get_settings] = get_settings_override
+    with TestClient(app) as test_client:
+        # Testing
+        yield test_client
+
+    # Tear down
+
+@pytest.fixture(scope = "module")
+def test_app_with_db():
+    # Set up
+    app = create_application()
+    app.dependency_overrides[get_settings] = get_settings_override
+    register_tortoise(
+        app,
+        db_url = os.environ.get("DATABASE_TEST_URL"),
+        modules = {"models": ["app.models.tortoise"]},
+        generate_schemas = True,
+        add_exception_handlers = True
+    )
     with TestClient(app) as test_client:
         # Testing
         yield test_client
