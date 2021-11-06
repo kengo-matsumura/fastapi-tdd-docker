e008c82c706b46803c4fd64460c73ecbcdcff07e
Kengo Matsumura <kengo.matsumura@outlook.com>
2021-11-06 23:54:12 +1100

    Added GET routes for a single summary request and for all summaries

--- project/app/api/crud.py
+++ project/app/api/crud.py
@@ -1,5 +1,6 @@
 from app.models.pydantic import SummaryPayloadSchema
 from app.models.tortoise import TextSummary
+from typing import Optional, List
 
 
 async def post(payload: SummaryPayloadSchema) -> int:
@@ -9,3 +10,13 @@ async def post(payload: SummaryPayloadSchema) -> int:
     )
     await summary.save()
     return summary.id
+
+async def get(id: int) -> Optional[dict]:
+    summary = await TextSummary.filter(id = id).first().values()
+    if summary:
+        return summary[0]
+    return None
+
+async def get_all() -> List:
+    summaries = await TextSummary.all().values()
+    return summaries
